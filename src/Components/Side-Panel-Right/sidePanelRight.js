e902f1619ff1f372a943a8d81960a2627945035a
Abhinav Rajput <rajputabhinav85@gmail.com>
2019-04-05 09:43:18 +0530

    Second Commit

--- src/Components/Side-Panel-Right/sidePanelRight.js
+++ src/Components/Side-Panel-Right/sidePanelRight.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
-
-
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import './sidePanelRight.css';
 
 class SidePanelRight extends Component{
     
     render(){
-        console.log(this.props);
+        console.log(this.props.userList[0]);
         return(
             <div className = 'side-panel-right'>
-                SidePanelRight
+                {this.props.userList.map((userInfo) => (
+                    <Card key = {userInfo.id}>
+                        <CardContent>
+                            <p>{userInfo.firstName}</p>
+                            <p>{userInfo.lastName}</p>
+                            <p>{userInfo.date}</p>
+                            <button onClick = {() => {this.props.removeCardById(userInfo.id)}}>Delete</button>
+                        </CardContent>
+                    </Card>
+                ))}
             </div>
         )
     }
--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,6 @@ import SidePanelRight from './Components/Side-Panel-Right/sidePanelRight';
 
 
 import './main.css';
-import { array } from 'prop-types';
 
 
 class Main extends Component{
@@ -49,23 +48,47 @@ class Main extends Component{
     }
 
     onSubmitData = () => {
-        let timeNow  = Math.round((new Date()).getTime() / 1000);
-        console.log(this.state.userInfo);
+        let timeNow  = Math.round((new Date()).getTime());
+
         let userInfo = this.state.userInfo;
+        userInfo = {...userInfo};
         userInfo.id = timeNow;
         let userList = this.state.userList;
         userList.push(userInfo);
+        
+        userList = [...userList];
+       
+
+        userInfo = {...userInfo};
         userInfo.firstName = '';
         userInfo.lastName = '';
         userInfo.date = this.getDate();
+
+        this.onSubmitClick(userInfo,userList)
+        
+        
+        // console.log('userList',userList);
+    }
+
+    onSubmitClick = (userInfo,userList) => {
         this.setState({
             userList : userList,
             userInfo : userInfo
         })
+
+    }    
+
+    removeCardById = (id) => {
+        console.log(id);
+        let userList = this.state.userList;
+        userList = userList.filter((userInfo) => userInfo.id !== id)
+
+        this.setState({
+            userList : userList
+        })
+        console.log(userList);
         
-        console.log('userList',userList);
     }
-    
     handleChange = name => event => {
         let userInfo = this.state.userInfo;
         userInfo[name] = event.target.value;
@@ -90,7 +113,9 @@ class Main extends Component{
                         handleChange = {this.handleChange}
                         onSubmitData = {this.onSubmitData}
                         /> 
-                    <SidePanelRight userList = {this.state.userList}/>
+                    <SidePanelRight 
+                        userList = {this.state.userList}
+                        removeCardById = {this.removeCardById}/>
                 </div>
             </div>
         )
